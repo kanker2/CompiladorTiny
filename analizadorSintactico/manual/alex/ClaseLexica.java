7afe3a721e2e540a636d28ef109fcc40e44d29de
karce <karce@ucm.es>
2024-03-02 17:45:12 +0100

    que si está tonto

---
+++ analizadorSintactico/manual/alex/ClaseLexica.java
@@ -0,0 +1,28 @@
+package alex;
+
+public enum ClaseLexica {
+ IDEN, 
+ ENT, 
+ REAL, 
+ PAP("("), 
+ PCIERRE(")"), 
+ IGUAL("="), 
+ COMA(","), 
+ MAS("+"), 
+ MENOS("-"), 
+ POR("*"), 
+ DIV("/"), 
+ EVALUA("<evalua>"), 
+ DONDE("<donde>"), 
+ EOF("<EOF>");
+private String image;
+public String getImage() {
+     return image;
+ }
+ private ClaseLexica() {
+     image = toString();
+ }
+ private ClaseLexica(String image) {
+    this.image = image;  
+ }
+}
