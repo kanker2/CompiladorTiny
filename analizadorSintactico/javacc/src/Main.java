9924957e64064f8a6fd9db18032697cfe434ae46
karce <karce@ucm.es>
2024-03-03 22:05:28 +0100

    no lo hace  a mi no m mientas

--- analizadorSintactico/javacc/src/Main.java
+++ analizadorSintactico/javacc/src/Main.java
@@ -5,6 +5,7 @@ import asint.AnalizadorSintacticoTiny;
 public class Main{
    public static void main(String[] args) throws Exception {
       AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(new FileReader(args[0]));
+      asint.disable_tracing();
       asint.programa_tiny();;
    }
 }
