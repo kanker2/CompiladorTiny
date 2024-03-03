ca0cad2f7b09fcab58dfd46a940c32a2d27fa090
LunaSantos11 <91669808+LunaSantos11@users.noreply.github.com>
2024-03-03 22:14:39 +0100

    ns si estará bien la spec

--- analizadorSintactico/cup/asint/Main.java
+++ analizadorSintactico/cup/asint/Main.java
@@ -4,14 +4,26 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import alex.AnalizadorLexicoEval;
+import alex.AnalizadorLexicoTiny;
+import errorsTiny0.GestionErroresTiny0.ErrorLexico;
+import errorsTiny0.GestionErroresTiny0.ErrorSintactico;
 
 public class Main {
    public static void main(String[] args) throws Exception {
      Reader input = new InputStreamReader(new FileInputStream(args[0]));
-	 AnalizadorLexicoEval alex = new AnalizadorLexicoEval(input);
-	 AnalizadorSintacticoEval asint = new AnalizadorSintacticoEval(alex);
+	 AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
+	 AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(alex);
 	 //asint.setScanner(alex);
 	 asint.parse();
+	 
+	 
+	 /*try {
+	    	 asint.analiza();
+	     } catch(ErrorSintactico e) {
+	    	 System.out.println("ERROR_SINTACTICO");
+	     } catch(ErrorLexico e) {
+	    	 System.out.println("ERROR_LEXICO");
+	     }*/
  }
 }   
    
