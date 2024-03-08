b29b61c380cc07ccf11068d09cf3a6bd1e7796e1
BonitoFish <dchen01@ucm.es>
2024-03-08 19:37:31 +0100

    pequeños retoques

--- programaPrincipal/Main.java
+++ programaPrincipal/Main.java
@@ -10,63 +10,54 @@ import cupAsint.*;
 import errors.GestionErroresTiny.ErrorLexico;
 import errors.GestionErroresTiny.ErrorSintactico;
 
-
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		 if (args.length < 2) {
-	            System.out.println("Error: Debes proporcionar el archivo y la opción (desc/asc).");
-	            return;
-	        }
-	     boolean error =false;
-	     //String archivo = args[0];
-	     String archivo = "C:/Users/juand/eclipse-workspace/proyluna/src/in_tiny0_2.txt";
-	     String opcion = args[1];
-
-	     //"C://Users/Luna Santos/eclipse-workspace/ProgramaPrincipal/src/in_tiny0_1.txt"
-		Reader input = new InputStreamReader(new FileInputStream(args[0]));
-			
-		
-		if(opcion.equalsIgnoreCase("desc")) {
-			
-			  AnalizadorSintacticoTinyDJ asint = new AnalizadorSintacticoTinyDJ(input);
-		      asint.disable_tracing();
-		       
-		      try {
-		    	  asint.programa_tiny();
-		      }
-		      catch(ParseException e) {
-		         System.out.println("ERROR_SINTACTICO");
-			 error =true;
-		      }
-		      catch(TokenMgrError e) {
-		         System.out.println("ERROR_LEXICO"); 
-			 error =true;
-		      }
-		    
+		if (args.length < 2) {
+			System.out.println("Error: Debes proporcionar el archivo y la opción (desc/asc).");
+			return;
 		}
-		else if (opcion.equalsIgnoreCase("asc")){
-			
+		boolean error = false;
+		// String archivo = args[0];
+		String opcion = args[1];
+		Reader input = new InputStreamReader(new FileInputStream(args[0]));
+
+		if (opcion.equalsIgnoreCase("desc")) {
+
+			AnalizadorSintacticoTinyDJ asint = new AnalizadorSintacticoTinyDJ(input);
+			asint.disable_tracing();
+
+			try {
+				asint.programa_tiny();
+			} catch (ParseException e) {
+				System.out.println("ERROR_SINTACTICO");
+				error = true;
+			} catch (TokenMgrError e) {
+				System.out.println("ERROR_LEXICO");
+				error = true;
+			}
+
+		} else if (opcion.equalsIgnoreCase("asc")) {
+
 			AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
 			AnalizadorSintacticoTinyDJCup asint = new AnalizadorSintacticoTinyDJCup(alex);
-			//asint.setScanner(alex);
+			// asint.setScanner(alex);
 
 			try {
 				asint.debug_parse();
 			} catch (ErrorLexico e) {
 				System.out.println("ERROR_LEXICO");
-				error =true;
-				
-				
+				error = true;
+
 			} catch (ErrorSintactico e) {
 				System.out.println("ERROR_SINTACTICO");
-				error =true;
+				error = true;
 			}
-		}
-		else {
+		} else {
 			System.out.println("Error, tipo de analizador sintactico no valido");
 		}
-		if(!error) System.out.println("OK");
+		if (!error)
+			System.out.println("OK");
 	}
 
 }
