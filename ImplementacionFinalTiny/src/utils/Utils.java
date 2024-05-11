1f898dbfdeed16bf3ae22cd321e590235e682386
BonitoFish <dchen01@ucm.es>
2024-05-11 19:21:02 +0200

    nose

--- ImplementacionFinalTiny/src/utils/Utils.java
+++ ImplementacionFinalTiny/src/utils/Utils.java
@@ -10,14 +10,15 @@ import asint.SintaxisAbstractaTiny.Tipo_definido;
 
 public class Utils {
 	static public Tipo ref(Tipo t) {
-    	if (t instanceof Tipo_definido)
-    		return ref(t.vinculo.tipo);
-    	return t;
+		if (t instanceof Tipo_definido)
+			return ref(t.vinculo.tipo);
+		return t;
 	}
-	
+
 	static public boolean es_designador(Exp e) {
 		if (e instanceof Iden || e instanceof Indexacion || e instanceof Acc_reg || e instanceof Indireccion)
 			return true;
 		return false;
 	}
+
 }
